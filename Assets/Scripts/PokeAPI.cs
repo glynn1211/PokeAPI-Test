ca9397324d49a539e78813107dcef1709e7c3c57
Glynn <glynn.merryweather@dbpixelhouse.com>
2021-10-24 18:27:46 +0100

    Added SimpleJSON to parse the JSON

--- Assets/Scripts/PokeAPI.cs
+++ Assets/Scripts/PokeAPI.cs
@@ -4,7 +4,15 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Networking;
 using UnityEngine.UI;
+using SimpleJSON;
+
+public class Pokemon
+{
+    string name;
+    string url;
+}
 
 public class PokeAPI : MonoBehaviour
 {
@@ -15,18 +23,43 @@ public class PokeAPI : MonoBehaviour
     TextMeshProUGUI pokeName, pokeNum;
     [SerializeField]
     Button randomButton;
+    [SerializeField]
+    TMP_Dropdown pokemonSelector;
 
 
-    private readonly string baseURL = "https://poekap.co/api/v2/";
+
+
+    private readonly string baseURL = "https://pokeapi.co/api/v2/";
 
     private void Start()
     {
-        randomButton.onClick.AddListener(ButtonPressed);
+        randomButton.onClick.AddListener(()=> {
+            ButtonPressed();
+        });
+        StartCoroutine("InitalRequest");
+        
+
     }
 
-    private void ButtonPressed()
+    private IEnumerator InitalRequest()
     {
-        //JsonUtility.FromJson<>();
+        UnityWebRequest pokedex = UnityWebRequest.Get($"{baseURL}pokemon?limit=1000");
+
+        yield return pokedex.SendWebRequest();
+
+        if(pokedex.result == UnityWebRequest.Result.ConnectionError || pokedex.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.LogError(pokedex.error);
+            yield break;
+        }
+        JSONNode pokemonInfo = JSON.Parse(pokedex.downloadHandler.text);
+         //pokemon = JsonUtility.FromJson<Pokemon[]>(pokedex.downloadHandler.text);
+        //Debug.Log(pokemon.Length);
+    }
+
+    private void ButtonPressed(int pokeNum = 0)
+    {
+        //Unity Web Request call to get the required information from the api.
 
     }
 }
